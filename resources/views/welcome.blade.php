dcd10209f22bf3e79ce6c8fdb6f405ab738bf5d4
Abednego Wambua <abedxh@gmail.com>
2019-09-10 16:45:14 +0300

    updated welcome links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,30 @@
 
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Vue Router Message Example From Scratch - ItSolutionStuff.com</title>
+    <title>Suppliers & Products (Supply Chain ) Simulation</title>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id="app">
-    <router-view></router-view>
+    <div class="container">
+        <h1>Suppliers & Products (Supply Chain ) Simulation</h1>
+
+        <nav>
+            <ul>
+                <li class="nav-item">
+                    <router-link to="/products">Products</router-link>
+                <li class="nav-item">
+                    <router-link to="/suppliers">Suppliers</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/orders">Orders</router-link>
+                </li>
+                </li>
+            </ul>
+        </nav>
+        <ProductsComponent></ProductsComponent>
+        <router-view></router-view>
+    </div>
 </div>
 <script src="{{asset('js/app.js')}}"></script>
 </body>
